dd489744609f4017648669a2b6513c8b433dbb9e
Risos8200 <risos8200@gmail.com>
2017-09-15 01:02:57 +0530

    Removing comments

--- app/src/main/java/com/example/risos8200/basiccalc/MainActivity.java
+++ app/src/main/java/com/example/risos8200/basiccalc/MainActivity.java
@@ -11,15 +11,16 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     public TextView editText;
     public String bT = "0", bt;
-    int a = 0,d=0;public Button clc;
-    public float num1 = 0, num2 = 0,res=0,dom=0.1f,m=0;
+    int a = 0, d = 0;
+    public Button clc;
+    public float num1 = 0, num2 = 0, res = 0, dom = 0.1f, m = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.final_layout);
         editText = (TextView) findViewById(R.id.editxt);
-        clc=(Button)findViewById(R.id.buttonDelete);
+        clc = (Button) findViewById(R.id.buttonDelete);
         editText.setText("0.0");
     }
 
@@ -29,36 +30,35 @@ public class MainActivity extends AppCompatActivity {
         bT = b.getText().toString();
         bT = bT.trim();
         if (bT.equals(".")) {
-           Log.d("bT",bT);
-                   d=1;
-            return(0);
+            Log.d("bT", bT);
+            d = 1;
+            return (0);
         }
         if (a == 0) {
             if (d == 0) {
                 num1 = num1 * 10 + Float.parseFloat(bT);
                 editText.setText(String.valueOf(num1));
             } else {
-                Log.d("i cam here","here");
-                num1 = num1 +dom*(Float.parseFloat(bT));
+                Log.d("i cam here", "here");
+                num1 = num1 + dom * (Float.parseFloat(bT));
                 editText.setText(String.valueOf(num1));
             }
-        }
-        else if(a==1){
+        } else if (a == 1) {
             if (d == 0) {
                 num2 = num2 * 10 + Float.parseFloat(bT);
-                editText.setText(String.valueOf(num2));}
-            else{
-                    num2 = num2 + dom * (Float.parseFloat(bT));
-                    editText.setText(String.valueOf(num2));
-                }
+                editText.setText(String.valueOf(num2));
+            } else {
+                num2 = num2 + dom * (Float.parseFloat(bT));
+                editText.setText(String.valueOf(num2));
             }
-            return(0);
+        }
+        return (0);
     }
 
     public void arth(View v) {
         clc.setText("DEL");
-        a=1;
-        d=0;
+        a = 1;
+        d = 0;
         Log.d("Num1", String.valueOf(num1));
         Button b = (Button) v;
         bt = b.getText().toString();
@@ -67,46 +67,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void equal(View v) {
-        if (bt.equals("+")){
-            res=num1 + num2;
-            editText.setText(String.valueOf(res));}
-        else if (bt.equals("-")){
-            res=num1 - num2;
-            editText.setText(String.valueOf(res));}
-        else if (bt.equals("*")){
-            res=num1 * num2;
-            editText.setText(String.valueOf(res));}
-        else if (bt.equals("/")){
-            res=num1 / num2;
-            editText.setText(String.valueOf(res));}
-        num1=res;
-        num2=0;
-        a=0;
+        if (bt.equals("+")) {
+            res = num1 + num2;
+            editText.setText(String.valueOf(res));
+        } else if (bt.equals("-")) {
+            res = num1 - num2;
+            editText.setText(String.valueOf(res));
+        } else if (bt.equals("*")) {
+            res = num1 * num2;
+            editText.setText(String.valueOf(res));
+        } else if (bt.equals("/")) {
+            res = num1 / num2;
+            editText.setText(String.valueOf(res));
+        }
+        num1 = res;
+        num2 = 0;
+        a = 0;
         clc.setText("C");
     }
-public void clear(View v){
-    m=0;
-    String cr=clc.getText().toString();
-    if(cr.equals("DEL")){
-        if (a == 0){
-            m=num1%10;
-            num1-=m;
-            num1/=10;
-            editText.setText(String.valueOf(num1));
-         }
-        else{
-            m=num2%10;
-            num2-=m;
-            num2/=10;
-            editText.setText(String.valueOf(num2));
-          }
+
+    public void clear(View v) {
+        m = 0;
+        String cr = clc.getText().toString();
+        if (cr.equals("DEL")) {
+            if (a == 0) {
+                m = num1 % 10;
+                num1 -= m;
+                num1 /= 10;
+                editText.setText(String.valueOf(num1));
+            } else {
+                m = num2 % 10;
+                num2 -= m;
+                num2 /= 10;
+                editText.setText(String.valueOf(num2));
+            }
+        } else if (cr.equals("C")) {
+            editText.setText("0.0");
+            num1 = 0;
+            num2 = 0;
+            clc.setText("DEL");
+        }
     }
-    else if(cr.equals("C"))
-    {
-        editText.setText("0.0");
-        num1=0;
-        num2=0;
-        clc.setText("DEL");
-    }
-}
 }
